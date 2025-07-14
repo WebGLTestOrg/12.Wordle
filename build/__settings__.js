2ec6e5e9ba605007a9b8362e4b683a76578930c0
Alex <92122843+Taduar2001@users.noreply.github.com>
2025-07-14 14:53:46 +0300

    Init

---
+++ build/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "./build/";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2267617.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 240020938, 236572765, 236572766, 236572767, 236572768, 236572769, 236572770, 240016188, 240016230, 240016700, 240016822, 240017893 ];
+window.CONFIG_FILENAME = window.ASSET_PREFIX +"config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
